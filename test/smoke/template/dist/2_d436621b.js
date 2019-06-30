662abc32e6a77afd2a8938247ae1755ce123edb3
cpselvis <cpselvis@gmail.com>
2019-06-30 16:18:53 +0800

    feat: builder travis ci

---
+++ test/smoke/template/dist/2_d436621b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{8:function(n,e,t){"use strict";t.r(e);var i=t(0),o=t.n(i);e.default=function(){return o.a.createElement("div",null,"动态 import")}}}]);
